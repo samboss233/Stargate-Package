1fae53edd32eb666b0ee5b8f9ffba4c6e8b8f054
Sammy Bossman <samboss233@gmail.com>
2026-01-12 18:16:53 -0500

    add app router layout + favicon

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Stargate',
+  icons: {
+    icon: '/favicon.png'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
